b901b076b1d69127f93971c3c0c0c264382ac6fb
HariThrive <hramgain@gmail.com>
2026-01-06 23:33:26 +0530

    corsConfiguration code

--- springSecurity/src/main/java/com/springSecurity/springSecurity/config/SecurityConfig.java
+++ springSecurity/src/main/java/com/springSecurity/springSecurity/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.springSecurity.springSecurity.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import com.springSecurity.springSecurity.utils.JwtFilter;
 
@@ -17,6 +21,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http,JwtFilter jwtFilter)throws Exception {
 		http.csrf(AbstractHttpConfigurer::disable)
+		.cors(cors -> cors.configurationSource(corsConfiguration()))
 			.authorizeHttpRequests(auth ->auth
 			.requestMatchers("/auth/**").permitAll()
 //			 .requestMatchers("/product/**").authenticated()
@@ -29,5 +34,20 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() throws Exception {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean // method used for communication b/w front and backend 
+	public CorsConfigurationSource corsConfiguration() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(List.of("*"));//orgin website 
+		configuration.setAllowedMethods(List.of("GET","POST","PUT"));
+		configuration.setAllowedHeaders(List.of("*"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		
+		source.registerCorsConfiguration("/**", configuration);
+		
+		return (CorsConfigurationSource) source;
+		
 	}
 }
